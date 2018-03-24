c7a21c6a54519676464a2ba81660f8ef8fd3560a
Prestige <vovashevchenko1998@ukr.net>
2018-03-24 12:12:51 +0200

    three_commit

--- Jewels/Jewels/Main.cpp
+++ Jewels/Jewels/Main.cpp
@@ -1,8 +1,24 @@
-#include <iostream>
-using namespace std;
+#include <SFML/Graphics.hpp>
 
-int main() {
-	cout << "Hello, world!" << endl;
-	system("pause");
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFMLworks");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
